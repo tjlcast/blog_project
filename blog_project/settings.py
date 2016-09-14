8ea3f7b8f7fdfaef5510a0be9c55d651fff7ab97
tangjialiang <13261711910@163.com>
2016-09-14 16:52:12 +0800

    demo_2

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default.png': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'USER': 'root',
