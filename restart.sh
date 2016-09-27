32765352ee05c33bad12bf97e07933fc080c079b
tangjialiang <13261711910@163.com>
2016-09-27 23:21:16 +0800

    add sh for service to start!!

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo 'is Stopping....'
-ps aux | grep 'manage' | awk '{print "kill -9 "$2";"}' | sh
+ps aux | grep 'manage.py' | awk '{print "kill -9 "$2";"}' | sh
 
 echo 'restart....'
 nohup /root/project/blog_old/manage.py runserver 0.0.0.0:8001 &
